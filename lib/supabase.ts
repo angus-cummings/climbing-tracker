b9d681cbf23e4a0571f5b9aa2bbb218085ae113a
angus-cummings <72483948+angus-cummings@users.noreply.github.com>
2025-12-17 23:58:26 +1100

    fixed superbase api

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -5,7 +5,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  //process.env.VITE_NEXT_PUBLIC_SUPABASE_URL!,
-  "https://hrkukqgdfkgxyvinzkqe.supabase.co",
-  "sb_publishable_ZOh4HeiBgHJf4BB3WYeqWw_4bnifiBc"
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
